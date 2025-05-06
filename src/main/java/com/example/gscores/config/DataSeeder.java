938aa90faac9c97387daec4cce1698453b325976
quocti2003 <lejack526@gmail.com>
2025-05-06 17:00:53 +0700

    Fix: Temporarily disable DataSeeder to diagnose OOM issue

--- src/main/java/com/example/gscores/config/DataSeeder.java
+++ src/main/java/com/example/gscores/config/DataSeeder.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class DataSeeder implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(DataSeeder.class);
