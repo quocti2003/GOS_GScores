b781312a179d15a1fbcf87cd206faf0402bd069b
Quốc Tỉ <101783021+quocti2003@users.noreply.github.com>
2025-05-07 10:19:38 +0700

    Update DataSeeder.java import annotation Value

--- src/main/java/com/example/gscores/config/DataSeeder.java
+++ src/main/java/com/example/gscores/config/DataSeeder.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 //@Component
